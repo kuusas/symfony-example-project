15e2fdd16b788970067bef30a5ec8282c454a4d8
Žilvinas Kuusas <zilvinas@kuusas.lt>
2016-04-09 21:01:50 +0300

    Custom error handler only for prod environment

--- src/AppBundle/EventListener/ExceptionListener.php
+++ src/AppBundle/EventListener/ExceptionListener.php
@@ -7,8 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ExceptionListener
 {
+    private $environment;
+
+    public function __construct($environment)
+    {
+        $this->environment = $environment;
+    }
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
+        if ($this->environment != 'prod') {
+            return;
+        }
         $exception = $event->getException();
         $response = new Response('Error!');
         // setup the Response object based on the caught exception
